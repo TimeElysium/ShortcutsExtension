a3a57d911adca7f2c3654cdcc0eacae87b392fc1
TimeElysium <67021200+TimeElysium@users.noreply.github.com>
2021-10-07 16:02:29 +0200

    Storage use and new shortcuts

--- Assets/data.js
+++
@@ -1,3 +0,0 @@
-const shortcuts = {"Manga": ["https://proxer.me/", "https://www.asurascans.com/", "https://reaperscans.com/", "https://kunmanga.com/", "https://mangarockteam.com/home/", "https://flamescans.org/", "https://gogoscans.com/", "https://lhtranslation.net/"],
-    "Testing": ["https://www.youtube.com/", "https://www.google.com/"],
-    "Tester": []};
--- Assets/popup.js
+++ Assets/popup.js
@@ -1,40 +1,61 @@
-// Read shortcut data and create needed buttons and shortcuts
-let shortcutsContainer = document.getElementById("container");
-for (let shortcutName in shortcuts) {
-  let shortcutRow = document.createElement("div");
-  shortcutRow.className = "shortcutRow";
-  shortcutRow.id = shortcutName;
-  let content = shortcuts[shortcutName];
+const scd = {"Manga": ["https://proxer.me/", "https://www.asurascans.com/", "https://reaperscans.com/", "https://kunmanga.com/", "https://mangarockteam.com/home/", "https://flamescans.org/", "https://gogoscans.com/", "https://lhtranslation.net/"],
+    "Testing": ["https://www.youtube.com/", "https://www.google.com/"],
+    "Tester": []};
 
-  // Create shortcut button
-  let shortcutButton = document.createElement("button");
-  shortcutButton.innerHTML = shortcutName;
-  shortcutButton.className = "shortcutBtn";
-  shortcutButton.onclick = function() {
-    for (let j = 0; j < content.length; j++) {
-      chrome.tabs.create({
-        url: content[j]
-      });
+/*
+chrome.storage.sync.set({"shortcutData": scd}, function() {});
+*/
+
+chrome.storage.sync.get("shortcutData", function(data) {
+  const shortcuts = data["shortcutData"];
+  
+  // Read shortcut data and create needed buttons and shortcuts
+  let shortcutsContainer = document.getElementById("container");
+  for (let shortcutName in shortcuts) {
+    let shortcutRow = document.createElement("div");
+    shortcutRow.className = "shortcutRow";
+    shortcutRow.id = shortcutName;
+    let content = shortcuts[shortcutName];
+
+    // Create shortcut button
+    let shortcutButton = document.createElement("button");
+    shortcutButton.innerHTML = shortcutName;
+    shortcutButton.className = "shortcutBtn";
+    shortcutButton.onclick = function() {
+      for (let j = 0; j < content.length; j++) {
+        chrome.tabs.create({
+          url: content[j]
+        });
+      }
     }
-  }
-  shortcutRow.appendChild(shortcutButton);
-  // Create shortcut edit button
-  let shortcutEditButton = document.createElement("button");
-  shortcutEditButton.className = "shortcutEdit";
-  shortcutEditButton.innerHTML = "Edit";
-  shortcutRow.appendChild(shortcutEditButton);
+    shortcutRow.appendChild(shortcutButton);
+    // Create shortcut edit button
+    let shortcutEditButton = document.createElement("button");
+    shortcutEditButton.className = "shortcutEdit";
+    shortcutEditButton.innerHTML = "Edit";
+    shortcutRow.appendChild(shortcutEditButton);
 
-  // Create shortcut delete button
-  let shortcutDeleteButton = document.createElement("button");
-  shortcutDeleteButton.className = "shortcutDelete";
-  shortcutDeleteButton.innerHTML = "X";
-  shortcutDeleteButton.onclick = function() {
-    if (confirm("Do you really want to delete this shortcut?")) {
-      document.getElementById(shortcutName).remove();
-      delete shortcuts[shortcutName]; // will be modified later on
-    } 
-  }
-  shortcutRow.appendChild(shortcutDeleteButton);
+    // Create shortcut delete button
+    let shortcutDeleteButton = document.createElement("button");
+    shortcutDeleteButton.className = "shortcutDelete";
+    shortcutDeleteButton.innerHTML = "X";
+    shortcutDeleteButton.onclick = function() {
+      if (confirm("Do you really want to delete this shortcut?")) {
+        document.getElementById(shortcutName).remove();
+        delete shortcuts[shortcutName]; // will be modified later on
+      } 
+    }
+    shortcutRow.appendChild(shortcutDeleteButton);
 
-  shortcutsContainer.appendChild(shortcutRow);
-}
+    shortcutsContainer.appendChild(shortcutRow);
+  }
+
+  let newShortcutButton = document.createElement("button");
+  newShortcutButton.id = "newShortcutButton";
+  newShortcutButton.innerHTML = "Create new Shortcut";
+  newShortcutButton.onclick = function() {
+    let newShortcut = prompt("Enter name of new shortcut:", "shortcutName");
+    alert(newShortcut);
+  }
+  shortcutsContainer.appendChild(newShortcutButton);
+});
